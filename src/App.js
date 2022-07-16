f12b02d077053c468ae7d87169c9030753faa088
David <braginsky.david@yandex.ru>
2022-07-16 20:11:37 +0300

    added link on homepage to add new transaction

--- src/App.js
+++ src/App.js
@@ -1,39 +1,13 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 function App() {
-  const [expense, setExpense] = useState("");
-  const [income, setIncome] = useState("");
   const [transactionList, setTransactionList] = useState([]);
-
-  const submitHandler = (e) => {
-    e.preventDefault();
-    console.log("form was submitted");
-    console.log("expense: ", expense);
-    console.log("income: ", income);
-  };
-
-  const expenseChangeHandler = (e) => {
-    setExpense(e.target.value);
-  };
-
-  const incomeChangeHandler = (e) => {
-    setIncome(e.target.value);
-  };
 
   return (
     <div className="container">
       <h1>Budget App</h1>
-      <form onSubmit={submitHandler}>
-        <div className="inputWrapper">
-          <label htmlFor="expense">Expense</label>
-          <input value={expense} onChange={expenseChangeHandler} type="number" id="expense" />
-        </div>
-        <div className="inputWrapper">
-          <label htmlFor="income">Income</label>
-          <input value={income} onChange={incomeChangeHandler} type="number" id="income" />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+      <Link to="/transaction">Add Transaction</Link>
     </div>
   );
 }
--- src/Views/Transaction.jsx
+++ src/Views/Transaction.jsx
@@ -1,7 +1,36 @@
+import { useState } from "react";
+
 const Transaction = () => {
+  const [expense, setExpense] = useState("");
+  const [income, setIncome] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("form was submitted");
+    console.log("expense: ", expense);
+    console.log("income: ", income);
+  };
+
+  const expenseChangeHandler = (e) => {
+    setExpense(e.target.value);
+  };
+
+  const incomeChangeHandler = (e) => {
+    setIncome(e.target.value);
+  };
   return (
     <>
-      <p>transaction page</p>
+      <form onSubmit={submitHandler}>
+        <div className="inputWrapper">
+          <label htmlFor="expense">Expense</label>
+          <input value={expense} onChange={expenseChangeHandler} type="number" id="expense" />
+        </div>
+        <div className="inputWrapper">
+          <label htmlFor="income">Income</label>
+          <input value={income} onChange={incomeChangeHandler} type="number" id="income" />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </>
   );
 };
