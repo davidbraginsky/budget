683e02f3176b649b9b65531c410aed269d47a1a0
David <braginsky.david@yandex.ru>
2022-07-16 19:41:41 +0300

    added mock layout form with income and expense fields

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Budget App</h1>
+      <form>
+        <div className="inputWrapper">
+          <label htmlFor="expense">Expense</label>
+          <input type="text" id="expense" />
+        </div>
+        <div className="inputWrapper">
+          <label htmlFor="income">Income</label>
+          <input type="text" id="income" />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
