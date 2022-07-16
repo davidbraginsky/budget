bf51a1d4c7bba2f1a2c72ac83f991bf30b5b1141
David <braginsky.david@yandex.ru>
2022-07-16 23:04:11 +0300

    added styling for transaction view with seperate options for expense/income/transfer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function App() {
-  const [transactionList, setTransactionList] = useState([]);
-
   return (
     <div className="container">
       <nav className="nav">
         <div className="sidebarIcon"></div>
         <p className="nav__title">Home</p>
       </nav>
+      <section>
+        <h1>List of Wallets</h1>
+      </section>
       <Link to="/transaction">Add Transaction</Link>
     </div>
   );
--- src/Views/Transaction.jsx
+++ src/Views/Transaction.jsx
@@ -1,8 +1,13 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const Transaction = () => {
   const [expense, setExpense] = useState("");
   const [income, setIncome] = useState("");
+  const [option, setOption] = useState("");
+
+  const incomeOptionRef = useRef(null);
+  const expenseOptionRef = useRef(null);
+  const transferOptionRef = useRef(null);
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -18,8 +23,41 @@ const Transaction = () => {
   const incomeChangeHandler = (e) => {
     setIncome(e.target.value);
   };
+
+  const resetOptionClasses = () => {
+    incomeOptionRef.current.classList.remove("option--active");
+    expenseOptionRef.current.classList.remove("option--active");
+    transferOptionRef.current.classList.remove("option--active");
+  };
+
+  const optionChangeHandler = (e) => {
+    resetOptionClasses();
+    const option = e.target.innerText.toLowerCase();
+    if (option === "income") {
+      incomeOptionRef.current.classList.add("option--active");
+    } else if (option === "expense") {
+      expenseOptionRef.current.classList.add("option--active");
+    } else if (option === "transfer") {
+      transferOptionRef.current.classList.add("option--active");
+    }
+  };
   return (
-    <>
+    <div className="transaction">
+      <div className="confirmation-row">
+        <i className="fa-solid fa-xmark"></i>
+        <i className="fa-solid fa-check"></i>
+      </div>
+      <div className="option-row">
+        <div ref={incomeOptionRef} onClick={optionChangeHandler} className="option">
+          Income
+        </div>
+        <div ref={expenseOptionRef} onClick={optionChangeHandler} className="option option--active">
+          Expense
+        </div>
+        <div ref={transferOptionRef} onClick={optionChangeHandler} className="option">
+          Transfer
+        </div>
+      </div>
       <form onSubmit={submitHandler}>
         <div className="inputWrapper">
           <label htmlFor="expense">Expense</label>
@@ -31,7 +69,7 @@ const Transaction = () => {
         </div>
         <button type="submit">Submit</button>
       </form>
-    </>
+    </div>
   );
 };
 
