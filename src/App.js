62790db518f0693dad6f66f591afa775968e30f0
David <braginsky.david@yandex.ru>
2022-07-16 19:48:30 +0300

    added ability to submit income and expenses in form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
+import { useState } from "react";
+
 function App() {
+  const [expense, setExpense] = useState("");
+  const [income, setIncome] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("form was submitted");
+    console.log("expense: ", expense);
+    console.log("income: ", income);
+  };
+
+  const expenseChangeHandler = (e) => {
+    setExpense(e.target.value);
+  };
+
+  const incomeChangeHandler = (e) => {
+    setIncome(e.target.value);
+  };
+
   return (
     <div className="container">
       <h1>Budget App</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className="inputWrapper">
           <label htmlFor="expense">Expense</label>
-          <input type="text" id="expense" />
+          <input value={expense} onChange={expenseChangeHandler} type="number" id="expense" />
         </div>
         <div className="inputWrapper">
           <label htmlFor="income">Income</label>
-          <input type="text" id="income" />
+          <input value={income} onChange={incomeChangeHandler} type="number" id="income" />
         </div>
         <button type="submit">Submit</button>
       </form>
